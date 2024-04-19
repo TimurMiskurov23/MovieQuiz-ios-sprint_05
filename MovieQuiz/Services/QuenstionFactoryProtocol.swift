f905e0854f0f794294ae27c84b3bc4c2f6b9258a
TimurMiskurov23 <timur.miskurov@yandex.ru>
2024-04-19 21:12:02 +0300

    Прокинул делегат Фабрики вопросов, почистил код

---
+++ MovieQuiz/Services/QuenstionFactoryProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  QuenstionFactoryProtocol.swift
+//  MovieQuiz
+//
+//  Created by Miskurov Timur on 10.04.2024.
+//
+
+import Foundation
---
+++ MovieQuiz/Services/QuestionFactoryDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  QuestionFactoryDelegate.swift
+//  MovieQuiz
+//
+//  Created by Miskurov Timur on 16.04.2024.
+//
+
+import Foundation
